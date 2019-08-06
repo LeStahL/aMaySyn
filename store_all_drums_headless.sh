07a7529522f0c66b6ceb0cb893bfef228caf34a9
qm210 <quantenmace@gmail.com>
2019-08-06 23:40:37 +0200

    Improved HEADLESS mode

---
+++ store_all_drums_headless.sh
@@ -0,0 +1,36 @@
+name='drums'
+synfile=$name'.syn'
+tempfile=$name'.may.template'
+destfile=$name'.may'
+outfile=$name'.tmp'
+
+rm 'out/'$name'_'*'.wav'
+
+sed -i 's/#maindrum/maindrum/' $synfile
+ndrums=$(grep maindrum $synfile | wc -l)
+
+echo $ndrums drums in $synfile
+
+for i in $(seq 1 $ndrums)
+do
+
+	sed 's/\$/'$i'/g' $tempfile > $destfile
+	
+	echo 'run '$synfile' with drum synth '$i
+	./run.sh $name -headless > $outfile
+
+	k=$(printf "%03d" $i)
+
+	drumname=$(grep 'ACTUALLY USED DRUMS' $outfile | sed -e "s/.*\['//" -e "s/'\]//")
+	mv -v 'out/'$name'_001.wav' 'out/_'$name'_'$drumname'.wav'
+done
+
+###################
+# UPCOMING IDEAS: #
+################### 
+#
+# - different vel / aux
+# - call template of randoms and store values
+#
+#
+#
